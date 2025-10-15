c66cf886817bd92758ed2cc799537c023b065909
jangse0 <syjang1016@korea.ac.kr>
2025-10-15 16:29:26 +0900

    25.10.15필수기능lv.1 완료

--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -6,39 +6,62 @@ public class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n1, n2;
+        while (true)
+        {
+            int n1, n2;
+            double result = 0;
 
-        while (true) {
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            n1 = scanner.nextInt();
-            if (n1<=0){
-                System.out.print("양의 정수(0포함)를 입력하세요.\n");
+            //양의 정수가 입력될 때까지 다시 입력받는다.
+            while (true) {
+                System.out.print("첫 번째 숫자를 입력하세요: ");
+                n1 = scanner.nextInt();
+                if (n1 < 0) {
+                    System.out.print("양의 정수(0포함)를 입력하세요.\n");
+                } else
+                    break;
             }
-            else
-                break;
-        }
 
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        n2 = scanner.nextInt();
+            while (true) {
+                System.out.print("두 번째 숫자를 입력하세요: ");
+                n2 = scanner.nextInt();
+                if (n2 < 0) {
+                    System.out.print("양의 정수(0포함)를 입력하세요.\n");
+                } else
+                    break;
+            }
 
-        System.out.print("사칙연산 기호를 입력하세요: ");
-        char simbol = scanner.next().charAt(0);
 
-        switch (simbol) {
-            case '+':
-                System.out.println(n1 + n2);
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            char symbol = scanner.next().charAt(0);
+
+            switch (symbol) {
+                case '+':
+                    result = n1 + n2;
+                    break;
+                case '-':
+                    result = n1 - n2;
+                    break;
+                case '*':
+                    result = n1 * n2;
+                    break;
+                case '/':
+                    if (n2 == 0) {
+                        System.out.println("0으로 나눌 수 없습니다.");
+                        continue;
+                    }
+                    result = (double)n1 / n2;
+                    break;
+                default:
+                    System.out.println("\n올바른 연산자(+, -, *, /)를 입력해주세요.\n");
+                    continue;
+            }
+
+
+            System.out.println("---------------\n결과는 "+ result);
+            System.out.print("더 계산하시겠습니까? (exit 입력 시 종료)");
+
+            if (scanner.next().equals("exit"))
                 break;
-            case '-':
-                System.out.println(n1 - n2);
-                break;
-            case '*':
-                System.out.println(n1 * n2);
-                break;
-            case '/':
-                System.out.println(n1 / n2);
-                break;
-            default:
-                System.out.println("+, -, *, / 중 하나를 입력하세요");
         }
     }
 }
