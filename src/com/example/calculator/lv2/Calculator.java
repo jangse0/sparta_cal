71855da89ab7d1f302b18598eb89cd6fd1230af1
jangse0 <syjang1016@korea.ac.kr>
2025-10-21 13:05:56 +0900

    lv2 주석추가

--- src/com/example/calculator/lv2/Calculator.java
+++ src/com/example/calculator/lv2/Calculator.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Calculator {
-
+    //연산 결과 저장
     private List<Double> results = new ArrayList<>();
 
     public double calculate(int a, int b, char operator)
